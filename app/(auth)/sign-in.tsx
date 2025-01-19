b75318ff2f8e284882583b6f66a4d8440d2e5935
Wilian <wsilvadev@gmail.com>
2025-01-18 22:00:34 -0300

    feat: create signUp and signIn using appwrite

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,13 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '@/constants'
 import FormField from '@/components/FormField'
 import CustomButton from '@/components/CustomButton'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
+import { signIn } from '../../lib/appwrite'
 
-const SingIn = () => {
+const SignIn = () => {
     const [form, setform] = useState({
         email: '',
         password: '',
@@ -14,7 +15,23 @@ const SingIn = () => {
 
     const [isSubmitting, setIsSubmitting] = useState(false)
 
-    const submit = () => {}
+    const submit = async () => {
+        if (!form.email || !form.password) {
+            Alert.alert('Error', 'Please enter all fields')
+        }
+        setIsSubmitting(true)
+        try {
+            const result = await signIn(form.email, form.password)
+            console.log('result', result)
+            // set it to global state
+
+            router.replace('/home')
+        } catch (error) {
+            Alert.alert('Error', error?.message)
+        } finally {
+            setIsSubmitting(false)
+        }
+    }
     return (
         <SafeAreaView className="bg-primary h-full">
             <ScrollView>
@@ -67,4 +84,4 @@ const SingIn = () => {
     )
 }
 
-export default SingIn
+export default SignIn
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,10 +1,11 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '@/constants'
 import FormField from '@/components/FormField'
 import CustomButton from '@/components/CustomButton'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
+import { createUser } from '../../lib/appwrite'
 
 const SignUp = () => {
     const [form, setform] = useState({
@@ -15,7 +16,23 @@ const SignUp = () => {
 
     const [isSubmitting, setIsSubmitting] = useState(false)
 
-    const submit = () => {}
+    const submit = async () => {
+        if (!form.username || !form.email || !form.password) {
+            Alert.alert('Error', 'Please enter all fields')
+        }
+        setIsSubmitting(true)
+        try {
+            // const result =
+            await createUser(form.email, form.password, form.username)
+            // set it to global state
+
+            router.replace('/home')
+        } catch (error) {
+            Alert.alert('Error', error?.message)
+        } finally {
+            setIsSubmitting(false)
+        }
+    }
     return (
         <SafeAreaView className="bg-primary h-full">
             <ScrollView>
